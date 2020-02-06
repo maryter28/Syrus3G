0a30fe1e45b4cb2097091605a927a270a10a8437
maryter28 <43224086+maryter28@users.noreply.github.com>
2020-02-06 14:12:03 -0500

    Add files via upload

---
+++ APP-ANDROID/app/build/generated/source/buildConfig/release/com/obdelm327pro/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.obdelm327pro;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.obdelm327pro";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "30.0";
+}
